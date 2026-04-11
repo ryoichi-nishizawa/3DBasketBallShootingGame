5743ab63a77f2aaabf8ade1552195082f47496c4
ryoichi-nishizawa <nishizawa.ryoichi.0309@gmail.com>
2026-04-11 20:50:46 +0900

    bug fixed of Goal Detector

--- 3DBasketBallShootingGame/Assets/Script/GoalDetector.cs
+++ 3DBasketBallShootingGame/Assets/Script/GoalDetector.cs
@@ -25,8 +25,37 @@ public class GoalDetector : MonoBehaviour
         // it collided was the ball.
         if (touch.gameObject.CompareTag("Ball"))
         {
+            Rigidbody rb = touch.GetComponent<Rigidbody>();
+            if (rb == null)
+            {
+                return;
+            }
+
+            // --- Solution 1: Ignore entry from below ---
+            // Process only when Y velocity is negative (= falling)
+            if (rb.linearVelocity.y > 0) 
+            {
+#if UNITY_EDITOR
+                Debug.Log("========== Invalid : Entered from below ==========");
+#endif
+                return;
+            }
+
+            // --- Solution 2: Ignore rim collisions ---
+            // Only trigger detection when the ball center is above the trigger center
+            // (Prevents false positives from bouncing off the rim or entering sideways)
+            if (touch.transform.position.y < transform.position.y)
+            {
+#if UNITY_EDITOR
+                Debug.Log("========== Invalid : Contact from side or bottom ==========");
+#endif
+                return;
+            }
+
             score++;
-            Debug.Log("========== score increases : "+score+" ==========");
+#if UNITY_EDITOR
+            Debug.Log("========== Score Increases : "+score+" ==========");
+#endif
             UpdateScoreUI();
         }
     }
